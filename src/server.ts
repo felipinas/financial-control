f9273aae079f3c49af39b0d23431794b554c4fde
felipinas <joaofelipemail99@gmail.com>
2024-01-05 15:20:27 -0300

    deploy: add host config in listen

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { env } from './env';
 app.listen(
   {
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   },
   () => console.log(`Running on ${env.PORT}`),
 );
